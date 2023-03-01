6da00032bb1c9275c626f1ea720fe43c43d2a936
Idrisov Daniyal <daniyalou@gmail.com>
2023-03-01 17:57:35 +0300

    user-table

---
+++ src/components/UserTable/User/User.vue
@@ -0,0 +1,174 @@
+<template>
+  <tr class="userTable__table-body-row">
+    <td class="userTable__table-body-row-column">
+      {{ id }}
+    </td>
+
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uName" />
+    </td>
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uUsername" />
+    </td>
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uEmail" />
+    </td>
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uStreet" />
+    </td>
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uCity" />
+    </td>
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uPhone" />
+    </td>
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uWebsite" />
+    </td>
+    <td class="userTable__table-body-row-column">
+      <input type="text" v-model="uComanyName" />
+    </td>
+
+    <td
+      class="userTable__table-body-row-column userTable__table-body-row-table-buttons"
+    >
+      <button
+        class="userTable__table-body-row-table-button button__update"
+        @click="update(id)"
+      >
+        Изменить
+      </button>
+      <button
+        class="userTable__table-body-row-table-button button__delete"
+        @click="deleteUser(id)"
+      >
+        Удалить
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "UserItem",
+
+  props: {
+    id: {
+      type: Number,
+    },
+    name: {
+      type: String,
+      default: "No Name",
+    },
+    username: {
+      type: String,
+      default: "No Username",
+    },
+    email: {
+      type: String,
+      default: "undefield@gmail.com",
+    },
+    address: {
+      type: Object,
+    },
+    phone: {
+      type: String,
+    },
+    website: {
+      type: String,
+      default: "google.com",
+    },
+    company: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      uName: "",
+      uUsername: "",
+      uEmail: "",
+      uStreet: "",
+      uCity: "",
+      uPhone: "",
+      uWebsite: "",
+      uComanyName: "",
+    };
+  },
+  methods: {
+    update(id) {
+      this.$emit("update", {
+        id: id,
+        name: this.uName,
+        username: this.uUsername,
+        email: this.uEmail,
+        street: this.uStreet,
+        city: this.uCity,
+        phone: this.uPhone,
+        website: this.uWebsite,
+        companyName: this.uComanyName,
+      });
+    },
+    deleteUser(id) {
+      this.$emit("deleteUser", id);
+    },
+  },
+  created() {
+    this.uName = this.name;
+    this.uUsername = this.username;
+    this.uEmail = this.email;
+    this.uStreet = this.address.street;
+    this.uCity = this.address.city;
+    this.uPhone = this.phone;
+    this.uWebsite = this.website;
+    this.uComanyName = this.company.name;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.userTable__table-body-row-column input {
+  font-size: 12px;
+  border: 1px solid #d4d4d4;
+  text-align: center;
+  margin-left: 2px;
+  height: 30px;
+  border-radius: 4px;
+}
+.userTable__table-body-row:last-child {
+  border-bottom: 1px;
+}
+.userTable__table-body-row-table-buttons {
+  display: flex;
+  flex-direction: column;
+}
+
+.userTable__table-body-row-table-button {
+  margin: 5px 5px;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  border: none;
+}
+.button__update {
+  background-color: #4caf50;
+  color: white;
+}
+
+.button__update:hover {
+  background-color: #3e8e41;
+}
+
+.button__delete {
+  background-color: #d12418;
+  color: white;
+}
+
+.button__delete:hover {
+  background-color: #6d1812;
+}
+</style>
---
+++ src/components/UserTable/UserTable.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="userTable">
+    <table class="userTable__table">
+      <thead class="userTable__table-head">
+        <tr class="userTable__table-head-row">
+          <th class="userTable__table-head-row-column">Id</th>
+          <th class="userTable__table-head-row-column">Name</th>
+          <th class="userTable__table-head-row-column">Username</th>
+          <th class="userTable__table-head-row-column">Email</th>
+          <th class="userTable__table-head-row-column">Street</th>
+          <th class="userTable__table-head-row-column">City</th>
+          <th class="userTable__table-head-row-column">Phone</th>
+          <th class="userTable__table-head-row-column">Website</th>
+          <th class="userTable__table-head-row-column">Company</th>
+          <th class="userTable__table-head-row-column">Active</th>
+        </tr>
+      </thead>
+      <tbody
+        v-for="(item, index) in users"
+        :key="item.id"
+        class="userTable__table-body"
+      >
+        <UserItem
+          :id="index"
+          :name="item.name"
+          :username="item.username"
+          :email="item.email"
+          :address="item.address"
+          :phone="item.phone"
+          :website="item.website"
+          :company="item.company"
+          @update="update"
+          @deleteUser="deleteUser"
+        />
+      </tbody>
+    </table>
+
+    <!-- <button @click="cons">click</button> -->
+  </div>
+</template>
+
+<script>
+import UserItem from "./User/User.vue";
+export default {
+  name: "UserTable",
+  components: { UserItem },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    cons() {
+      console.log(this.users);
+    },
+    update(data) {
+      this.users[data.id].name = data.name;
+      this.users[data.id].username = data.username;
+      this.users[data.id].email = data.email;
+      this.users[data.id].address.street = data.street;
+      this.users[data.id].address.city = data.city;
+      this.users[data.id].phone = data.phone;
+      this.users[data.id].website = data.website;
+      this.users[data.id].company.name = data.companyName;
+    },
+    deleteUser(index) {
+      console.log(index);
+
+      this.users.splice(index, 1);
+    },
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => (this.users = json));
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.userTable__table {
+  width: 100%;
+  border-collapse: collapse;
+  text-align: center;
+}
+.userTable__table-head {
+  background-color: #ffffff;
+}
+.userTable__table-body:nth-child(2n) {
+  background-color: #f2f2f2;
+}
+
+.userTable__table-head-row-column {
+  border: 1px solid rgb(192, 192, 192);
+  padding: 8px;
+}
+</style>
